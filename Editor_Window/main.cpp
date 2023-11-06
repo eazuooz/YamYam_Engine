97513dfd7ebb9e2de4b4e1549689e52722c478eb
eazuooz <aquarius4785@gmail.com>
2023-11-06 18:29:49 +0900

    깃허브 테스트용 첫 커밋

--- Editor_Window/main.cpp
+++ Editor_Window/main.cpp
@@ -25,6 +25,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
+    // 깃허브 테스트
+
     // TODO: 여기에 코드를 입력합니다.
 
     // 전역 문자열을 초기화합니다.
